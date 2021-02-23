99b622a4a645f93988228cabfc6a0b1fd5115ba8
arvind <arvind.bisht.90475@gmail.com>
2021-02-24 03:14:45 +1000

    updated firebase config file

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -13,14 +13,22 @@ const messagingSenderId = "919173591222";
 const appId = "1:919173591222:web:77fbcb3127c8b1e77c7617";
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_.apiKey || apiKey,
-  authDomain: process.env.REACT_APP_.authDomain || authDomain,
-  databaseURL: process.env.REACT_APP_.databaseURL || databaseURL,
-  projectId: process.env.REACT_APP_.projectId || projectId,
-  storageBucket: process.env.REACT_APP_.storageBucket || storageBucket,
-  messagingSenderId:
-    process.env.REACT_APP_.messagingSenderId || messagingSenderId,
-  appId: process.env.REACT_APP_.appId || appId,
+  // apiKey: process.env.REACT_APP_.apiKey || apiKey,
+  // authDomain: process.env.REACT_APP_.authDomain || authDomain,
+  // databaseURL: process.env.REACT_APP_.databaseURL || databaseURL,
+  // projectId: process.env.REACT_APP_.projectId || projectId,
+  // storageBucket: process.env.REACT_APP_.storageBucket || storageBucket,
+  // messagingSenderId:
+  //   process.env.REACT_APP_.messagingSenderId || messagingSenderId,
+  // appId: process.env.REACT_APP_.appId || appId,
+
+  apiKey: "AIzaSyBhsKQlNBUGwc6LguB1NsCzSrvve3mWwqw",
+  authDomain: "react-auth-56a8f.firebaseapp.com",
+  databaseURL: "https://react-auth-56a8f-default-rtdb.firebaseio.com",
+  projectId: "react-auth-56a8f",
+  storageBucket: "react-auth-56a8f.appspot.com",
+  messagingSenderId: "919173591222",
+  appId: "1:919173591222:web:77fbcb3127c8b1e77c7617",
 };
 
 // Initialize Firebase

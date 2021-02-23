9231918bfe90139c40b00c6df30cd759dd2fa271
arvind <arvind.bisht.90475@gmail.com>
2021-02-24 03:09:32 +1000

    added object for env file for publishing app

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -4,14 +4,23 @@ import "firebase/auth";
 import "firebase/firestore";
 import "firebase/storage";
 
+const apiKey = "AIzaSyBhsKQlNBUGwc6LguB1NsCzSrvve3mWwqw";
+const authDomain = "react-auth-56a8f.firebaseapp.com";
+const databaseURL = "https://react-auth-56a8f-default-rtdb.firebaseio.com";
+const projectId = "react-auth-56a8f";
+const storageBucket = "react-auth-56a8f.appspot.com";
+const messagingSenderId = "919173591222";
+const appId = "1:919173591222:web:77fbcb3127c8b1e77c7617";
+
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_.apiKey,
-  authDomain: process.env.REACT_APP_.authDomain,
-  databaseURL: process.env.REACT_APP_.databaseURL,
-  projectId: process.env.REACT_APP_.projectId,
-  storageBucket: process.env.REACT_APP_.storageBucket,
-  messagingSenderId: process.env.REACT_APP_.messagingSenderId,
-  appId: process.env.REACT_APP_.appId,
+  apiKey: process.env.REACT_APP_.apiKey || apiKey,
+  authDomain: process.env.REACT_APP_.authDomain || authDomain,
+  databaseURL: process.env.REACT_APP_.databaseURL || databaseURL,
+  projectId: process.env.REACT_APP_.projectId || projectId,
+  storageBucket: process.env.REACT_APP_.storageBucket || storageBucket,
+  messagingSenderId:
+    process.env.REACT_APP_.messagingSenderId || messagingSenderId,
+  appId: process.env.REACT_APP_.appId || appId,
 };
 
 // Initialize Firebase
